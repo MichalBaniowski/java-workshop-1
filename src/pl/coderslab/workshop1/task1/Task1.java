9545b606bd6f9cdce4c627e6a97da6fcc7b90c1f
Michał Baniowski <michal.baniowski88@gmail.com>
2018-12-15 23:36:04 +0100

    task1

--- src/pl/coderslab/workshop1/task1/Task1.java
+++ src/pl/coderslab/workshop1/task1/Task1.java
@@ -1,7 +1,32 @@
 package pl.coderslab.workshop1.task1;
 
+import java.util.Random;
+import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args){
+        int random = new Random().nextInt(100)+1;
+        int guess = 0;
+        int tryCount = 0;
+        do{
+            tryCount++;
+            guess = getNumber("podaj liczbę");
+            if(guess > random){
+                System.out.println("za dużo");
+            }
+            else if(guess < random){
+                System.out.println("za mało");
+            }
+        }while (guess != random);
+        System.out.format("brawo zadłeś w %d próbie", tryCount);
+    }
+    static int getNumber(String prompt){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(prompt);
 
+        while (!scanner.hasNextInt()){
+            scanner.nextLine();
+            System.out.println(prompt);
+        }
+        return scanner.nextInt();
     }
 }
